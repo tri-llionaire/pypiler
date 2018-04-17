39824a7b8c0c608c078e609d119850714e84512a
Tristan Price <33553882+tri-llionaire@users.noreply.github.com>
2018-04-17 14:59:36 -0400

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -2,6 +2,7 @@
 cmdict = {'print': 'CMD(PRINT)', 'raw_input': 'CMD(RAWINPUT)'}
 commands = ['print']
 nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '_', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 program = raw_input('TOKENIZE:\n')
 program = program.split()
 for i in program:
@@ -159,6 +160,10 @@ for i in program:
     elif i == '/':
         program = ' '.join(program)
         program = program.replace(i, 'SLASH ')
+        program = program.split()
+    elif i in chars:
+        program = ' '.join(program)
+        program = program.replace(i, 'CHAR(%s) ' % i)
         program = program.split()
     else:
         program = ' '.join(program)
