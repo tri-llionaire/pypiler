5a0364a66026a512b6af847db65480d46b0076c6
Tristan Price <33553882+tri-llionaire@users.noreply.github.com>
2018-04-18 07:47:45 -0400

    Create lexer.py

---
+++ 2/lexer.py
@@ -0,0 +1,60 @@
+#LEXER 2.17
+program = raw_input('TOKENIZE:\n')
+lst = program.split()
+tokens = []
+op = {
+    '~': 'TILDE',
+    '`': 'GRAVE',
+    '!': 'EXCL',
+    '@': 'ATSIGN',
+    '#': 'HASH',
+    '$': 'DOLLAR',
+    '%': 'PERCENT',
+    '^': 'CARET',
+    '&': 'ANDSIGN',
+    '*': 'STAR',
+    '(': 'LPAREN',
+    ')': 'RPAREN',
+    '-': 'MINUS',
+    '+': 'PLUS`',
+    '=': 'EQUAL',
+    '{': 'LBRACE',
+    '}': 'RBRACE',
+    '[': 'LBRACK',
+    ']': 'RBRACK',
+    '|': 'PIPE',
+    '\'': 'QUOTE',
+    '\"': 'DQUOTE',
+    ':': 'COLON',
+    ';': 'SEMIC',
+    '.': 'PERIOD',
+    ',': 'COMMA',
+    '\\': 'BSLASH',
+    '<': 'LARROW',
+    '>': 'RARROW',
+    '?': 'QUESTION',
+    '/': 'SLASH',
+}
+for i in lst:
+    if len(i) > 1:
+        if i.startswith('raw_input(\''):
+            tokens.append('CMD(INPUT)')
+            tokens.append('STR(%s)' % i[11:-2])
+        elif i.startswith('raw_input('):
+            tokens.append('CMD(INPUT)')
+            tokens.append('ID(%s)' % i[10:-1])
+        elif i.startswith('print \''):
+            tokens.append('CMD(PRINT)')
+            tokens.append('STR(%s)' % i[7:-1])
+        else:
+            tokens.append('ID({})'.format(i))
+    elif i.isdigit():
+        tokens.append('NUM({})'.format(i))
+    elif i.isalpha():
+        tokens.append('CHAR({})'.format(i))
+    elif i in op:
+        tokens.append(op[i])
+    else:
+        tokens = ['Error']
+        break
+print ' '.join(tokens)
